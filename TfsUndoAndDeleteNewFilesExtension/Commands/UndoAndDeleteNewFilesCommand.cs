92f63c069f83b52c8ef65915267b14a843c3b0aa
Denis Belik <denbell712@gmail.com>
2023-02-19 17:58:00 +0200

    Add extension

---
+++ TfsUndoAndDeleteNewFilesExtension/Commands/UndoAndDeleteNewFilesCommand.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.VisualStudio.TeamFoundation.VersionControl;
+using System.IO;
+using System.Linq;
+
+namespace TfsUndoAndDeleteNewFilesExtension
+{
+    [Command(PackageIds.UndoAndDeleteNewFilesCommand)]
+    internal sealed class UndoAndDeleteNewFilesCommand : BaseCommand<UndoAndDeleteNewFilesCommand>
+    {
+        private VersionControlExt _versionControlExtension;
+
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            // Warning VSTHRD010
+            // Accessing "DTE" should only be done on the main thread. Await JoinableTaskFactory.SwitchToMainThreadAsync() first
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            EnvDTE.DTE dte = Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
+
+            _versionControlExtension = dte.GetObject(typeof(VersionControlExt).FullName) as VersionControlExt;
+
+            var pendingChangesBefore = GetPendingChangesOfTypeAdd();
+
+            var executed = await VS.Commands.ExecuteAsync("TeamFoundationContextMenus.PendingChangesPageChangestoInclude.TfsContextPendingChangesPageUndo");
+
+            if (!executed)
+            {
+                // log
+                return;
+            }
+
+            var pendingChangesAfter = GetPendingChangesOfTypeAdd();
+
+            var undoneChanges = pendingChangesBefore.Except(pendingChangesAfter).ToList();
+
+            foreach (var pendingChange in undoneChanges)
+            {
+                if (File.Exists(pendingChange.LocalItem))
+                {
+                    File.Delete(pendingChange.LocalItem);
+                }
+            }
+        }
+
+        private PendingChange[] GetPendingChangesOfTypeAdd()
+        {
+            return _versionControlExtension.PendingChanges.IncludedChanges
+                .Where(x => x.ChangeType.HasFlag(ChangeType.Add))
+                .Concat(_versionControlExtension.PendingChanges.ExcludedChanges
+                    .Where(x => x.ChangeType.HasFlag(ChangeType.Add)))
+                .ToArray();
+        }
+    }
+}
