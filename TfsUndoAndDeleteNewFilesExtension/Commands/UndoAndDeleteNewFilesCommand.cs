cada2c1ad7a826d2baa9b482ccc3c72a11fa8f90
Denis Belik <denbell712@gmail.com>
2023-02-19 19:37:35 +0200

    add output pane writing

--- TfsUndoAndDeleteNewFilesExtension/Commands/UndoAndDeleteNewFilesCommand.cs
+++ TfsUndoAndDeleteNewFilesExtension/Commands/UndoAndDeleteNewFilesCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.TeamFoundation.VersionControl.Client;
 using Microsoft.VisualStudio.TeamFoundation.VersionControl;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -9,37 +10,86 @@ namespace TfsUndoAndDeleteNewFilesExtension
     internal sealed class UndoAndDeleteNewFilesCommand : BaseCommand<UndoAndDeleteNewFilesCommand>
     {
         private VersionControlExt _versionControlExtension;
+        private OutputWindowPane _outputWindowPane;
 
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
-            // Warning VSTHRD010
-            // Accessing "DTE" should only be done on the main thread. Await JoinableTaskFactory.SwitchToMainThreadAsync() first
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-            EnvDTE.DTE dte = Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
+            await InitOutputWindowPaneAsync();
+            await InitVersionControlExtAsync();
 
-            _versionControlExtension = dte.GetObject(typeof(VersionControlExt).FullName) as VersionControlExt;
+            await LogAsync($"Executing {this.GetType().Name} [{DateTime.Now}]");
 
             var pendingChangesBefore = GetPendingChangesOfTypeAdd();
+            await LogAsync($"Found {pendingChangesBefore.Length} 'adds' before undo");
 
-            var executed = await VS.Commands.ExecuteAsync("TeamFoundationContextMenus.PendingChangesPageChangestoInclude.TfsContextPendingChangesPageUndo");
+            string commandName = "TeamFoundationContextMenus.PendingChangesPageChangestoInclude.TfsContextPendingChangesPageUndo";
+            await LogAsync($"Executing {commandName}");
+            var executed = await VS.Commands.ExecuteAsync(commandName);
 
             if (!executed)
             {
-                // log
+                await LogAsync($"Execution failed, aborting");
                 return;
             }
+            else
+                await LogAsync($"Execution succeeded");
 
             var pendingChangesAfter = GetPendingChangesOfTypeAdd();
+            await LogAsync($"Found {pendingChangesAfter.Length} 'adds' after undo");
 
-            var undoneChanges = pendingChangesBefore.Except(pendingChangesAfter).ToList();
+            var undoneChanges = pendingChangesBefore.Except(pendingChangesAfter).ToArray();
+            await LogAsync($"Found {undoneChanges.Length} undone 'adds'");
 
             foreach (var pendingChange in undoneChanges)
             {
                 if (File.Exists(pendingChange.LocalItem))
                 {
+                    await LogAsync($"Deleting file {pendingChange.LocalItem}");
                     File.Delete(pendingChange.LocalItem);
                 }
+                else
+                {
+                    await LogAsync($"File {pendingChange.LocalItem} not found, skipping deletion");
+                }
             }
+        }
+
+        private async Task LogAsync(string message)
+        {
+            if (_outputWindowPane == null)
+            {
+                Trace.TraceError($"'{_outputWindowPane}' is null");
+            }
+
+            var lines = message.Split(
+                new string[] { Environment.NewLine },
+                StringSplitOptions.None);
+
+            foreach (var line in lines)
+            {
+                await _outputWindowPane.WriteLineAsync(line);
+            }
+        }
+
+        private async Task InitOutputWindowPaneAsync()
+        {
+            var tfsSourceControlOutputPaneGuid = new Guid("0EE13505-C0BC-40C3-8F1D-0F65A9F71616");
+            _outputWindowPane = await VS.Windows.GetOutputWindowPaneAsync(tfsSourceControlOutputPaneGuid);
+            if (_outputWindowPane == null)
+            {
+                // if can't get tfs pane, create our own
+                _outputWindowPane = await VS.Windows.CreateOutputWindowPaneAsync("TfsUndoAndDeleteNewFilesExtension");
+            }
+            await _outputWindowPane.ActivateAsync();
+        }
+
+        private async Task InitVersionControlExtAsync()
+        {
+            // Warning VSTHRD010
+            // Accessing "DTE" should only be done on the main thread. Await JoinableTaskFactory.SwitchToMainThreadAsync() first
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            EnvDTE.DTE dte = Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
+            _versionControlExtension = dte.GetObject(typeof(VersionControlExt).FullName) as VersionControlExt;
         }
 
         private PendingChange[] GetPendingChangesOfTypeAdd()
