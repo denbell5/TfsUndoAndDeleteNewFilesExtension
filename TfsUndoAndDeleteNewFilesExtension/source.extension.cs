d225121775c5484281d5d6fa1e26f19f86b2458a
denbell5 <denbell712@gmail.com>
2023-03-23 10:25:55 +0200

    fix description and bump version

--- TfsUndoAndDeleteNewFilesExtension/source.extension.cs
+++ TfsUndoAndDeleteNewFilesExtension/source.extension.cs
@@ -10,10 +10,9 @@ namespace TfsUndoAndDeleteNewFilesExtension
         public const string Id = "TfsUndoAndDeleteNewFilesExtension.dc12e21c-f5a9-4d77-b5f7-4d8642858c42";
         public const string Name = "TfsUndoAndDeleteNewFilesExtension";
         public const string Description = @"Visual Studio extension that performs undo of pending changes and deletes new (added) files
-Visual Studio extension that performs undo of pending changes and deletes new (added) files
 ";
         public const string Language = "en-US";
-        public const string Version = "1.1";
+        public const string Version = "1.2";
         public const string Author = "Denys Belik";
         public const string Tags = "";
     }
